d7adc1e5be4e556ecad1e2a1abe2802e5e978d4c
김희망 <gmlakd990514@naver.com>
2024-10-21 09:51:35 +0900

    init: init

---
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/SsacthreeShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.ssacthree_shop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsacthreeShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsacthreeShopApiApplication.class, args);
+    }
+
+}
