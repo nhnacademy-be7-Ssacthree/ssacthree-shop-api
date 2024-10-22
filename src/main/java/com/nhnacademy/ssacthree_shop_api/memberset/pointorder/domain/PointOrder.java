efc25fc0b3f9775795b66509267bd177d75bab47
김희망 <gmlakd990514@naver.com>
2024-10-22 16:17:04 +0900

    feat: 리뷰, 포인트리뷰, 포인트오더, 포인트반품 엔티티 추가

---
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/memberset/pointorder/domain/PointOrder.java
@@ -0,0 +1,31 @@
+package com.nhnacademy.ssacthree_shop_api.memberset.pointorder.domain;
+
+import com.nhnacademy.ssacthree_shop_api.memberset.pointhistory.domain.PointHistory;
+import com.nhnacademy.ssacthree_shop_api.order.domain.Order;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name="point_order")
+public class PointOrder {
+
+    @NotNull
+    @Id
+    private long id;
+
+
+    @MapsId
+    @OneToOne
+    @JoinColumn(name="point_history_id")
+    private PointHistory pointHistory;
+
+    @OneToOne
+    @JoinColumn(name="order_id")
+    private Order order;
+}
