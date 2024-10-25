26238eb3e68c36a4933534848fa54b3c6974c16c
김희망 <gmlakd990514@naver.com>
2024-10-25 19:23:23 +0900

    feat: customer service 테스트 완료

--- src/test/java/com/nhnacademy/ssacthree_shop_api/customer/service/CustomerServiceTest.java
+++ src/test/java/com/nhnacademy/ssacthree_shop_api/customer/service/CustomerServiceTest.java
@@ -7,12 +7,15 @@ import static org.mockito.Mockito.when;
 import com.nhnacademy.ssacthree_shop_api.customer.domain.Customer;
 import com.nhnacademy.ssacthree_shop_api.customer.dto.CustomerCreateRequest;
 import com.nhnacademy.ssacthree_shop_api.customer.dto.CustomerUpdateRequest;
+import com.nhnacademy.ssacthree_shop_api.customer.exception.CustomerNotFoundException;
 import com.nhnacademy.ssacthree_shop_api.customer.repository.CustomerRepository;
 import com.nhnacademy.ssacthree_shop_api.customer.service.impl.CustomerServiceImpl;
 import com.nhnacademy.ssacthree_shop_api.memberset.membergrade.domain.MemberGrade;
 import com.nhnacademy.ssacthree_shop_api.memberset.membergrade.dto.MemberGradeUpdateResponse;
+import com.nhnacademy.ssacthree_shop_api.memberset.membergrade.exception.MemberGradeNotFoundException;
 import java.util.Optional;
 import org.hibernate.validator.constraints.ru.INN;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -34,7 +37,8 @@ public class CustomerServiceTest {
 
         //given
 
-        CustomerCreateRequest dummy = new CustomerCreateRequest("test","010-1111-1111","test@naver.com");
+        CustomerCreateRequest dummy = new CustomerCreateRequest("test", "010-1111-1111",
+            "test@naver.com");
 
         //when
         customerService.createCustomer(dummy);
@@ -48,18 +52,91 @@ public class CustomerServiceTest {
 
         //given
 
-        CustomerUpdateRequest dummy = new CustomerUpdateRequest("test","010-1111-1111","test@naver.com");
-        Customer existingDummy = new Customer("test","010-1111-1111","test@naver.com");
-
-
+        CustomerUpdateRequest dummy = new CustomerUpdateRequest("test", "010-1111-1111",
+            "test@naver.com");
+        Customer existingDummy = new Customer("test", "010-1111-1111", "test@naver.com");
 
         when(customerRepository.findById(1L)).thenReturn(Optional.of(existingDummy));
         when(customerRepository.existsById(1L)).thenReturn(true);
-
         // when
         customerService.updateCustomer(1L, dummy);
+    }
 
+    @Test
+    void updateCustomerExceptionTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            customerService.updateCustomer(-1L, null);
+        });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            customerService.updateCustomer(0L, null);
+        });
 
+        Assertions.assertThrows(CustomerNotFoundException.class, () -> {
+            when(customerRepository.existsById(1L)).thenReturn(false);
+            customerService.updateCustomer(1L, null);
+        });
+    }
 
+    @Test
+    void deleteCustomer() {
+
+        //given
+        Long customerId = 1L;
+        Customer existingDummy = new Customer("test", "010-1111-1111", "test@naver.com");
+        when(customerRepository.existsById(1L)).thenReturn(true);
+
+        //when
+        customerService.deleteCustomerById(customerId);
+
+        //then
+        verify(customerRepository).deleteById(1L);
+
+    }
+
+    @Test
+    void deleteCustomerException() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            customerService.deleteCustomerById(-1L);
+        });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            customerService.deleteCustomerById(0L);
+        });
+
+        Assertions.assertThrows(CustomerNotFoundException.class, () -> {
+            when(customerRepository.existsById(1L)).thenReturn(false);
+            customerService.deleteCustomerById(1L);
+        });
+    }
+
+    @Test
+    void getCustomerById() {
+        //given
+        Long customerId = 1L;
+        Customer existingDummy = new Customer("test", "010-1111-1111", "test@naver.com");
+        when(customerRepository.findById(1L)).thenReturn(Optional.of(existingDummy));
+        when(customerRepository.existsById(1L)).thenReturn(true);
+
+        //when
+        customerService.getCustomerById(1L);
+
+        //then
+        verify(customerRepository).findById(1L);
+    }
+
+    @Test
+    void getMemberGradeByIdException() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            customerService.getCustomerById(-1L);
+        });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            customerService.getCustomerById(0L);
+        });
+
+        Assertions.assertThrows(CustomerNotFoundException.class, () -> {
+            when(customerRepository.existsById(1L)).thenReturn(false);
+            customerService.getCustomerById(1L);
+        });
     }
 }
