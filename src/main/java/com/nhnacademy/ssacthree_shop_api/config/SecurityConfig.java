42438efedfbf4342a98696419662538e1e9e31c6
minji <qkralswl6125@naver.com>
2024-10-29 10:58:47 +0900

    fix: dev, prod 개발 환경 properties 나누기

--- src/main/java/com/nhnacademy/ssacthree_shop_api/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/config/SecurityConfig.java
@@ -3,21 +3,26 @@ package com.nhnacademy.ssacthree_shop_api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // TODO : 우선은 모든 요청에 대해 인증되지 않아도 접근할 수 있도록 허용해놨음. 나중에 고쳐야 함.
-        http.authorizeHttpRequests(authorizeRequests ->
-            authorizeRequests.requestMatchers("/api/**").permitAll());
-
-        http.csrf(AbstractHttpConfigurer::disable);
+        // 모든 요청에 대해 인증되지 않아도 접근할 수 있도록 허용
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/api/**", "/h2-console/**").permitAll()
+                                .anyRequest().authenticated() // 추가: 인증되지 않은 요청은 인증 필요
+                )
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
+                .headers(headers -> headers.frameOptions().disable()); // H2-console을 사용하기 위해 추가
 
         return http.build();
     }
