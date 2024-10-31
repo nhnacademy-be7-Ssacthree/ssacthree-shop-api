5d890467c586e23381ffd940acb98837a9796554
hoohsh <hoohsh@daum.net>
2024-10-31 19:57:31 +0900

    fix: category 엔티티 수정

--- src/main/java/com/nhnacademy/ssacthree_shop_api/bookset/category/domain/Category.java
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/bookset/category/domain/Category.java
@@ -30,22 +30,16 @@ public class Category {
 
     @NotNull
     @Setter
-    @Column(columnDefinition = "boolean default false")
     private boolean categoryIsUsed;
-    //todo: H2에서는 "boolean default false" 이거 괜찮지만
-    // MySQL에서는 "TINYINT(1) DEFAULT 0" 어떡하죠?
-    // 방법3: @Column private boolean categoryIsUsed = false;
 
 
     // 자기 참조
     @Setter
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "super_category_id")
-    //@JsonBackReference // 순환 참조 방지
     private Category superCategory;
 
     @OneToMany(mappedBy = "superCategory", cascade = CascadeType.ALL)
-    //@JsonManagedReference // 순환 참조 방지
     private List<Category> categories = new ArrayList<>();
 
     public Category(String categoryName, Category superCategory) {
@@ -54,7 +48,7 @@ public class Category {
     }
 
 
-    //todo: categoryIsUsed에서 getter가 작동을 안 해서 임시로 추가함.
+    //categoryIsUsed에서 getter가 작동을 안 해서 추가함.
     public boolean getCategoryIsUsed() {
         return categoryIsUsed;
     }
