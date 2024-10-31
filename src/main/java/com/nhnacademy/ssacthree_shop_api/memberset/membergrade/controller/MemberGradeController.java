6ef24bc1347b0908bffc0bcb399601db371dd4a8
김희망 <gmlakd990514@naver.com>
2024-10-31 17:35:39 +0900

    fix: 테스트 코드 삭제 및 url 수정

--- src/main/java/com/nhnacademy/ssacthree_shop_api/memberset/membergrade/controller/MemberGradeController.java
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/memberset/membergrade/controller/MemberGradeController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/memberGrades")
+@RequestMapping("/api/shop/memberGrades")
 @RequiredArgsConstructor
 public class MemberGradeController {
 
