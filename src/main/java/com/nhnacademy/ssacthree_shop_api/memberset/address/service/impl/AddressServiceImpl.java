d3fc7ba0221a2f9966368b64ae91e7daaa4d5c6c
조병현 <jbk28252@naver.com>
2024-11-04 19:16:58 +0900

    fix : 버그수정

--- src/main/java/com/nhnacademy/ssacthree_shop_api/memberset/address/service/impl/AddressServiceImpl.java
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/memberset/address/service/impl/AddressServiceImpl.java
@@ -24,5 +24,6 @@ public class AddressServiceImpl implements AddressService {
             addressCreateRequest.getAddressDetail(),
             addressCreateRequest.getAddressPostalNumber()
         );
+        return address;
     }
 }
