c8507182cf37e8509c205ade22318b503f7152d1
hoohsh <hoohsh@daum.net>
2024-11-05 17:25:30 +0900

    feat: 도서카테고리, 도서태그 레포지토리 구현, fix: CsvController 생성자 주입방식 @RequiredArgsConstructor로 변경

---
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/bookset/bookcategory/repository/BookCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.ssacthree_shop_api.bookset.bookcategory.repository;
+
+import com.nhnacademy.ssacthree_shop_api.bookset.bookcategory.domain.BookCategory;
+import com.nhnacademy.ssacthree_shop_api.bookset.bookcategory.domain.BookCategoryId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookCategoryRepository extends JpaRepository<BookCategory, BookCategoryId> {
+}
---
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/bookset/booktag/repository/BookTagRepository.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.ssacthree_shop_api.bookset.booktag.repository;
+
+import com.nhnacademy.ssacthree_shop_api.bookset.booktag.domain.BookTag;
+import com.nhnacademy.ssacthree_shop_api.bookset.booktag.domain.BookTagId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookTagRepository extends JpaRepository<BookTag, BookTagId> {
+}
