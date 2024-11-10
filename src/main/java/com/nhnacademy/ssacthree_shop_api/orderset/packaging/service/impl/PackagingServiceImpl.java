3ec5390b1c15794ef2ce3691ea6e5f3821f22532
minji <qkralswl6125@naver.com>
2024-11-11 02:49:38 +0900

    feat: 포장지 정책 view 기능 구현

---
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/orderset/packaging/service/impl/PackagingServiceImpl.java
@@ -0,0 +1,28 @@
+package com.nhnacademy.ssacthree_shop_api.orderset.packaging.service.impl;
+
+import com.nhnacademy.ssacthree_shop_api.orderset.packaging.domain.Packaging;
+import com.nhnacademy.ssacthree_shop_api.orderset.packaging.dto.PackagingGetResponse;
+import com.nhnacademy.ssacthree_shop_api.orderset.packaging.repository.PackagingRepository;
+import com.nhnacademy.ssacthree_shop_api.orderset.packaging.service.PackagingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PackagingServiceImpl implements PackagingService {
+    private final PackagingRepository packagingRepository;
+
+    @Override
+    public List<PackagingGetResponse> getAllPackaging() {
+        List<Packaging> packaging = packagingRepository.findAll();
+        //초기화 필요
+        List<PackagingGetResponse> packagingGetResponses = new ArrayList<>();
+        for (Packaging pack : packaging) {
+            packagingGetResponses.add(new PackagingGetResponse(pack.getPackagingName(), pack.getPackagingPrice(), pack.getPackagingImageUrl()));
+        }
+        return packagingGetResponses;
+    }
+}
