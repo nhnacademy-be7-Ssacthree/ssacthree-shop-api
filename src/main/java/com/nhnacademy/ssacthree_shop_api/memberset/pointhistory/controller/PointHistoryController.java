e6510a3d8ddbc19586af22956f9c510251f76205
김희망 <gmlakd990514@naver.com>
2024-11-17 21:52:39 +0900

    fix: point history controller url 수정

--- src/main/java/com/nhnacademy/ssacthree_shop_api/memberset/pointhistory/controller/PointHistoryController.java
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/memberset/pointhistory/controller/PointHistoryController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/shop/members/my-page/point-histories")
+@RequestMapping("/api/shop/members/point-histories")
 @RequiredArgsConstructor
 public class PointHistoryController {
 
