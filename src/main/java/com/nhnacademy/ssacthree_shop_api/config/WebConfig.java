6b50064d02b950d04acfe4bc22c61cc770b9032f
hoohsh <hoohsh@daum.net>
2024-11-26 00:22:07 +0900

    fix: cors 설정 추가

---
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.ssacthree_shop_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://www.ssacthree.shop")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
+
