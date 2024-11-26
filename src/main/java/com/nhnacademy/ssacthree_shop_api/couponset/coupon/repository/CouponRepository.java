0bf88bb1d6d8c8d0ad78eb680149bf7662f6fd5f
Lee Eo Jin <win929@naver.com>
2024-11-26 09:37:33 +0900

    feat: 임시

--- src/main/java/com/nhnacademy/ssacthree_shop_api/couponset/coupon/repository/CouponRepository.java
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/couponset/coupon/repository/CouponRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
-    Optional<Coupon> findCouponByCouponName(String couponName);
+    Optional<Coupon> findByCouponName(String couponName);
 }
