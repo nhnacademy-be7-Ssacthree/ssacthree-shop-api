b07c6dbdc7000fe2b7b9a78a1bd53966176ab9ae
김희망 <gmlakd990514@naver.com>
2024-12-03 10:32:20 +0900

    fix: 테스트 버그 수정

--- src/test/java/com/nhnacademy/ssacthree_shop_api/bookset/tag/controller/TagControllerTest.java
+++ src/test/java/com/nhnacademy/ssacthree_shop_api/bookset/tag/controller/TagControllerTest.java
@@ -1,29 +1,27 @@
 package com.nhnacademy.ssacthree_shop_api.bookset.tag.controller;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.ssacthree_shop_api.bookset.tag.dto.request.TagUpdateRequest;
 import com.nhnacademy.ssacthree_shop_api.bookset.tag.dto.response.TagInfoResponse;
 import com.nhnacademy.ssacthree_shop_api.bookset.tag.service.TagService;
-import com.nhnacademy.ssacthree_shop_api.commons.paging.PageRequestBuilder;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -32,7 +30,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(TagController.class)
 @AutoConfigureMockMvc(addFilters = false)
-@Import(PageRequestBuilder.class)
 class TagControllerTest {
 
     @Autowired
