fec6b5d678ff086974205a46a1bc88dcd7513a7b
hoohsh <hoohsh@daum.net>
2024-12-03 19:31:40 +0900

    fix: 카테고리 예외처리 추가, 카테고리/도서 커스텀 예외 처리 수정

--- src/main/java/com/nhnacademy/ssacthree_shop_api/bookset/category/exception/CategoryDepthNotFoundException.java
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/bookset/category/exception/CategoryDepthNotFoundException.java
@@ -1,7 +1,9 @@
 package com.nhnacademy.ssacthree_shop_api.bookset.category.exception;
 
-public class CategoryDepthNotFoundException extends RuntimeException {
+import com.nhnacademy.ssacthree_shop_api.commons.exception.CustomException;
+
+public class CategoryDepthNotFoundException extends CustomException {
     public CategoryDepthNotFoundException(int depth) {
-        super("깊이 "+ depth +"에서 카테고리가 존재하지 않습니다.");
+        super("깊이 "+ depth +"에서 카테고리가 존재하지 않습니다.", 404);
     }
 }
