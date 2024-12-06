eccd5009836d20bf1e01a3b2fa3db9c743a6c6c7
Lee Eo Jin <win929@naver.com>
2024-12-06 22:50:03 +0900

    feat: PointSaveRule Test 작성 완료

--- src/main/java/com/nhnacademy/ssacthree_shop_api/memberset/pointsaverule/repository/PointSaveRuleRepository.java
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/memberset/pointsaverule/repository/PointSaveRuleRepository.java
@@ -4,7 +4,7 @@ import com.nhnacademy.ssacthree_shop_api.memberset.pointsaverule.domain.PointSav
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PointSaveRuleRepository extends JpaRepository<PointSaveRule, Long> {
+public interface PointSaveRuleRepository extends JpaRepository<PointSaveRule, Long>, PointSaveRuleCustomRepository {
 
     // 만약 회원가입 이라는 정책이 여러개라면? 그땐 query dsl 가야됨. 사실상 가야되는게 맞는데 일단 구현
     Optional<PointSaveRule> findPointSaveRuleByPointSaveRuleName(String pointSaveRuleName);
--- src/main/java/com/nhnacademy/ssacthree_shop_api/memberset/pointsaverule/service/impl/PointSaveRuleServiceImpl.java
+++ src/main/java/com/nhnacademy/ssacthree_shop_api/memberset/pointsaverule/service/impl/PointSaveRuleServiceImpl.java
@@ -3,16 +3,11 @@ package com.nhnacademy.ssacthree_shop_api.memberset.pointsaverule.service.impl;
 import com.nhnacademy.ssacthree_shop_api.memberset.pointsaverule.domain.PointSaveRule;
 import com.nhnacademy.ssacthree_shop_api.memberset.pointsaverule.dto.PointSaveRuleCreateRequest;
 import com.nhnacademy.ssacthree_shop_api.memberset.pointsaverule.dto.PointSaveRuleGetResponse;
-import com.nhnacademy.ssacthree_shop_api.memberset.pointsaverule.domain.QPointSaveRule;
 import com.nhnacademy.ssacthree_shop_api.memberset.pointsaverule.dto.PointSaveRuleInfoResponse;
 import com.nhnacademy.ssacthree_shop_api.memberset.pointsaverule.dto.PointSaveRuleUpdateRequest;
 import com.nhnacademy.ssacthree_shop_api.memberset.pointsaverule.exception.PointSaveRuleNotFoundException;
 import com.nhnacademy.ssacthree_shop_api.memberset.pointsaverule.repository.PointSaveRuleRepository;
 import com.nhnacademy.ssacthree_shop_api.memberset.pointsaverule.service.PointSaveRuleService;
-import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jakarta.ws.rs.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,29 +18,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PointSaveRuleServiceImpl implements PointSaveRuleService {
 
-    @PersistenceContext
-    private EntityManager entityManager;
-
     private final PointSaveRuleRepository pointSaveRuleRepository;
 
     @Override
     public List<PointSaveRuleGetResponse> getAllPointSaveRules() {
-        QPointSaveRule pointSaveRule = QPointSaveRule.pointSaveRule;
-
-        return new JPAQueryFactory(entityManager)
-                .select(Projections.constructor(
-                        PointSaveRuleGetResponse.class,
-                        pointSaveRule.pointSaveRuleId,
-                        pointSaveRule.pointSaveRuleName,
-                        pointSaveRule.pointSaveAmount,
-                        pointSaveRule.pointSaveRuleGenerateDate,
-                        pointSaveRule.pointSaveRuleIsSelected,
-                        pointSaveRule.pointSaveType
-                ))
-                .from(pointSaveRule)
-                .orderBy(pointSaveRule.pointSaveRuleIsSelected.desc())
-                .orderBy(pointSaveRule.pointSaveRuleGenerateDate.asc())
-                .fetch();
+        return pointSaveRuleRepository.getAllPointSaveRules();
     }
 
     @Override
